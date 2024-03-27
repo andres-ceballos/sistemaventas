d497c2793ed759280896a8840234c3d292fe7f0c
andres-ceballos <cr.andresf@gmail.com>
2024-03-26 23:39:24 -0500

    Login form

---
+++ CapaPresentacion/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Inicio form = new Inicio();
+
+            form.Show();
+            this.Hide();
+
+            form.FormClosing += frm_Closing;
+         
+        }
+
+        private void frm_Closing(object sender, FormClosingEventArgs e)
+        {
+            txtDocumento.Text = "";
+            txtContrasena.Text = "";
+            this.Show();
+        }
+    }
+}
